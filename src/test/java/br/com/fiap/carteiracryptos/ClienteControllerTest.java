01b61d0a9082b3b2ed70073f9b3207e08b08deb2
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-12-01 23:59:09 -0300

    Configura banco de dados h2 para testes e cria casos de teste para ClienteController

---
+++ src/test/java/br/com/fiap/carteiracryptos/ClienteControllerTest.java
@@ -0,0 +1,48 @@
+package br.com.fiap.carteiracryptos;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ClienteControllerTest {
+
+    @Test
+    public void deveCriarCliente() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"nome\": \"Alexandre\"}")
+                .when()
+                .post("/cliente")
+                .then().statusCode(201);
+    }
+
+    @Test
+    public void deveListarClienteInserido() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"nome\": \"Alexandre\"}")
+                .post("/cliente");
+        when()
+                .get("/cliente/lista")
+                .then().statusCode(200).assertThat().body("size()", is(1))
+                .body(containsString("\"nome\":\"Alexandre\""));
+    }
+
+    @Test
+    public void deveConsultarClienteInserido() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"nome\": \"Alexandre\"}")
+                .post("/cliente");
+        when()
+                .get("/cliente/1")
+                .then().statusCode(302)
+                .assertThat().body(containsString("\"nome\":\"Alexandre\""));
+    }
+}
